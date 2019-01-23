ec8171cffca6fcf4d52e6490ab914f27eb4b009f
Dmytro Katrychuk <pseudowolf.vn@gmail.com>
2019-01-23 13:16:44 -0600

    Add Marker class to incapsulate head marker tracking related code

--- eet_data_converter/head_mov_tracker.py
+++ eet_data_converter/head_mov_tracker.py
@@ -8,68 +8,82 @@ from skimage.io import imread
 
 from utils.img_path_gen import ImgPathGenerator
 
-def init_pattern(img, x, y):
-    return np.array(img[x - 4: x + 5, y - 5: y + 6], dtype=float)
+class Marker:
+    def __init__(self, img, x, y, width=11, height=9):
+        self.x = x
+        self.y = y
 
-def pattern_dist(img, x, y, pattern):
-    return np.linalg.norm(
-        np.array(img[x - 4: x + 5, y - 5: y + 6], dtype=float).flatten() -
-            pattern.flatten(),
-        ord=2
-    )
+        self.w = width
+        self.h = height
+
+        self.pattern = np.array(
+            img[x - self.h // 2: x + self.h // 2 + 1,
+                y - self.w // 2: y + self.w // 2 + 1],
+            dtype=float
+        ).flatten()
+
+    def dist(self, other):
+        return np.linalg.norm(self.pattern - other.pattern, ord=2)
+
+    def update(self, next_img):
+        min_dist = np.inf
+
+        # the search region is the window of 'W'x'W' size 
+        # centered around '(self.x, self.y)'
+        W = 31
+
+        # protect the search space to not run out of image boundaries 
+        for x in range(
+                max(self.x - W // 2, 0),
+                min(self.x + W // 2 + 1, next_img.shape[0] - self.h // 2)
+            ):
+            for y in range(
+                    max(self.y - W // 2, 0), 
+                    min(self.y + W // 2 + 1, next_img.shape[1] - self.w // 2)
+                ):
+                x = int(round(x))
+                y = int(round(y))
+                dist = self.dist(Marker(next_img, x, y))
+                if dist < min_dist:
+                    min_dist = dist
+                    min_x, min_y = x, y
+        
+        self.img = next_img
+        self.x = min_x
+        self.y = min_y
 
 # TODO: this doesn't work and right now initial marker position is determined
 # manually, come up with a better automatic algorithm
-def find_marker(img):
-    min_dist = np.inf
+# def find_marker(img):
+#     min_dist = np.inf
 
-    pattern_to_find = np.array([
-        [0., 0., 0., 0., 0., 0., 1., 1., 1., 1., 1.],
-        [0., 0., 0., 0., 0., 0., 1., 1., 1., 1., 1.],
-        [0., 0., 0., 0., 0., 0., 1., 1., 1., 1., 1.],
-        [0., 0., 0., 0., 0., 0., 1., 1., 1., 1., 1.],
-        [0., 0., 0., 0., 0., 0., 1., 1., 1., 1., 1.],
-        [1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.],
-        [1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.],
-        [1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.],
-        [1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.],
-    ])
+#     marker_to_find = Marker(np.array([
+#         [0., 0., 0., 0., 0., 0., 1., 1., 1., 1., 1.],
+#         [0., 0., 0., 0., 0., 0., 1., 1., 1., 1., 1.],
+#         [0., 0., 0., 0., 0., 0., 1., 1., 1., 1., 1.],
+#         [0., 0., 0., 0., 0., 0., 1., 1., 1., 1., 1.],
+#         [0., 0., 0., 0., 0., 0., 1., 1., 1., 1., 1.],
+#         [1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.],
+#         [1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.],
+#         [1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.],
+#         [1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.],
+#     ]), 4, 5)
 
-    coords = corner_peaks(corner_harris(img), min_distance=5)
-    coords_subpix = corner_subpix(img, coords, window_size=11)
+#     coords = corner_peaks(corner_harris(img), min_distance=5)
+#     coords_subpix = corner_subpix(img, coords, window_size=11)
 
-    for coord in coords_subpix:
-        x, y = coord
-        if np.isnan(x) or np.isnan(y):
-            continue
-        x = int(round(x))
-        y = int(round(y))
-        dist = pattern_dist(img, x, y, pattern_to_find) 
-        if dist < min_dist:
-            min_dist = dist
-            min_x, min_y = x, y
+#     for coord in coords_subpix:
+#         x, y = coord
+#         if np.isnan(x) or np.isnan(y):
+#             continue
+#         x = int(round(x))
+#         y = int(round(y))
+#         dist = marker_to_find.dist(Marker(img, x, y))
+#         if dist < min_dist:
+#             min_dist = dist
+#             min_x, min_y = x, y
 
-    return min_x, min_y
-
-def update_marker(img, prev_x, prev_y, prev_pattern):
-    min_dist = np.inf
-
-    # the search region is the window of 'W'x'W' size 
-    # centered around 'prev_center'
-    W = 31
-
-    for x in range(prev_x - W // 2, prev_x + W // 2 + 1):
-        for y in range(prev_y - W // 2, prev_y + W // 2 + 1):
-            if np.isnan(x) or np.isnan(y):
-                continue
-            x = int(round(x))
-            y = int(round(y))
-            dist = pattern_dist(img, x, y, prev_pattern) 
-            if dist < min_dist:
-                min_dist = dist
-                min_x, min_y = x, y
-    
-    return min_x, min_y
+#     return min_x, min_y
 
 def track_subj_marker(subj_root, visualize=False):
     print('Tracking head marker for subject: ' + subj_root)
@@ -79,22 +93,21 @@ def track_subj_marker(subj_root, visualize=False):
     with open(os.path.join(img_paths.get_root(), '_MarkerPos.txt')) as f:
         start_x, start_y = map(int, f.readline().split(' '))
 
-    mark_x = start_x
-    mark_y = start_y
-    mark_pattern = None
-
     # TODO: rewrite it in the way that allows to do it simultaneously
     # with all preprocessing steps
-    head_mov_path = os.path.join(img_paths.get_root(), 'head_mov.txt')
+    head_mov_path = os.path.join(img_paths.get_root(), 'head_mov_1.txt')
     head_mov_file = open(head_mov_path, 'w')
 
+    marker = None
     for img_path in img_paths:
         img = imread(img_path, as_gray=True)
-        if mark_pattern is None:
-            mark_pattern = init_pattern(img, start_x, start_y)
+        if marker is None:
+            marker = Marker(img, start_x, start_y)
         else:
-            mark_x, mark_y = update_marker(img, mark_x, mark_y, mark_pattern)
-        print(start_x - mark_x, start_y - mark_y, file=head_mov_file)
+            # will serve as the marker from the previos image
+            # for the next iteration
+            marker.update(img)
+        print(start_x - marker.x, start_y - marker.y, file=head_mov_file)
 
         # TODO: get rid of opencv for visualization
         if visualize:
@@ -109,7 +122,6 @@ def track_subj_marker(subj_root, visualize=False):
 def track_markers(dataset_root):
     for subj_root in os.listdir(dataset_root):
         track_subj_marker(os.path.join(dataset_root, subj_root))
-        break
 
 if __name__ == "__main__":
     track_markers(sys.argv[1])
