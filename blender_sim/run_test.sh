bd947fc9b6580a48a6d9e78ea3cb32e59b6b1ef6
Dmytro Katrychuk <pseudowolf.vn@gmail.com>
2019-01-28 21:40:04 -0600

    Remove legacy or unused code (related to synthetic simulation frameworks, i.e. Blender)

--- blender_sim/run_test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-python run_sim.py --root 'etdata' --source 'em_sign_convention' --design 'jazz'
--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from eyelink_data_converter.to_blender import convert_to_blender
+import sys
+
+from eet_data_converter.to_blender import preprocess
+from eet_data_converter.restore_missed import restore_missed_samples
+from eet_data_converter.head_mov_tracker import track_markers
 
 if __name__ == '__main__':
-    convert_to_blender()
+    # TODO: merge following to a separate preprocessing function
+    root = sys.argv[1]
+    restore_missed_samples(root)
+    track_markers(root)
+    preprocess(root)
