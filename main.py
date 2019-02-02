0274e3339754b97e575ac6b4952829aa5a99498f
Dmytro Katrychuk <pseudowolf.vn@gmail.com>
2019-02-02 02:55:40 -0600

    Add help docs for each argument

--- main.py
+++ main.py
@@ -12,32 +12,87 @@ from plots.boxplots_per_subject import plot_boxplots
 from plots.error_bars import plot_error_bars
 from plots.samples_distrib import draw_samples
 
+def add_subjs_argument(parser, arg_name, help):
+    parser.add_argument(arg_name, metavar='SUBJ', nargs='*', type=str, help=help)
+
+def add_archs_argument(parser):
+    archs = ['mlp', 'cnn']
+    parser.add_argument('--arch', default=archs, nargs='?', choices=archs,
+        help='''restrict corresponding analysis to 
+        either MLP or CNN architecture. If not specified, run for both.'''
+    )
+
+def add_setups_argument(parser):
+    setups = ['lp', 'hp']
+    parser.add_argument('--setup', default=setups, nargs='?', choices=setups,
+        help='''restrict corresponding analysis to 
+        either LP (low-power) or HP (high-power) setup.
+        If not specified, run for both.'''
+    )
+
 def build_subparsers():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--root', default='dataset', nargs='?')
+    parser.add_argument('--root', default='dataset', nargs=1,
+        help='''path to the dataset root directory.
+        If not specified, considered to be '.\dataset'.
+        Is used for the 'preproc', 'ml' modes and
+        'plot' mode with --samples_distrib''')
     subparsers = parser.add_subparsers(dest='cmd')
 
     preproc = subparsers.add_parser('preproc')
     ml = subparsers.add_parser('ml')
     plot = subparsers.add_parser('plot')
 
-    preproc.add_argument('--missed', nargs='*', type=str)
-    preproc.add_argument('--head_mov', nargs='*', type=str)
-    preproc.add_argument('--shift_crop', nargs='*', type=str)
-    preproc.add_argument('--psog', nargs='*', type=str)
+    add_subjs_argument(preproc, '--missed',
+        '''for every 'SUBJ' subject, fill each timestamp the signal from 
+        the eye-tracker was lost with NaNs. 
+        Run on the whole dataset, if no subjects specified'''
+    )
+    add_subjs_argument(preproc, '--head_mov',
+        '''for every 'SUBJ' subject, estimate head movements by tracking 
+        angular marker and save shifts for each timestamp to 
+        'ROOT\\SUBJ\\images\\head_mov.txt' file.
+        Run on the whole dataset, if no subjects specified'''
+    )
+    add_subjs_argument(preproc, '--shift_crop',
+        '''for every 'SUBJ' subject, load estimated head movements and 
+        with artificially added sensor shifts, crop appropriate image region
+        to get 'inside-headset-like' close eye captions and save them to
+        'ROOT\\SUBJ\\images_crop' directory.
+        Run on the whole dataset, if no subjects specified'''
+    )
+    add_subjs_argument(preproc, '--psog',
+        '''for every 'SUBJ' subject, simulate PSOG sensor outputs and save them 
+        together with eye-movement signal 
+        to 'ROOT\\SUBJ\\SUBJ_<PSOG.arch>.csv' file.
+        Run on the whole dataset, if no subjects specified'''
+    )
 
-    ml.add_argument('--grid_search', default=False, action='store_true')
-    ml.add_argument('--train', default=False, action='store_true')
-    archs = ['mlp', 'cnn']
-    ml.add_argument('--arch', default=archs, nargs='*', choices=archs)
-    setups = ['lp', 'hp']
-    ml.add_argument('--setup', default=setups, nargs='*', choices=setups)
+    ml.add_argument('--grid_search', default=False, action='store_true',
+        help='''run the grid-search to find the best architecture parameters.
+        Save the log with results to '.\\ml\\log' directory'''
+    )
+    ml.add_argument('--evaluate', default=False, action='store_true',
+        help='''evaluate best architectures found during grid-search 
+        for 'fine-tune' and 'from scratch' approaches on the whole dataset''')
+    add_archs_argument(ml)
+    add_setups_argument(ml)
     
-    plot.add_argument('--boxplots', default=False, action='store_true')
-    plot.add_argument('--error_bars', default=False, action='store_true')
-    plot.add_argument('--arch', default=archs, nargs='*', choices=archs)
-    plot.add_argument('--setup', default=setups, nargs='*', choices=setups)
-    plot.add_argument('--samples_distrib', nargs='*')
+    plot.add_argument('--boxplots', default=False, action='store_true',
+        help='''create additional boxplots of spatial accuracy for per subject 
+        evaluation of 'fine-tune' and 'from scratch' approaches and save them to
+        '.\\plots\\boxplots' directory''')
+    plot.add_argument('--error_bars', default=False, action='store_true',
+        help='''create error bars of spatial accuracy for evaluation of
+        'fine-tune' and 'from scratch' approaches and save them to
+        '.\\plots\\error_bars' directory''')
+    plot.add_argument('--samples_distrib', nargs='*',
+        help='''for every 'SUBJ' subject, create image that depicts samples
+        distribution on the screen with attempt to resemble calibration data split
+        and save them to '.\\plots\\samples_distrib' directory.
+        Run on the whole dataset, if no subjects specified''')
+    add_archs_argument(plot)
+    add_setups_argument(plot)
 
     return parser
 
